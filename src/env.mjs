d5c1fb87ff1f3b1dc28759f97a190f906d42bd0a
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-09-25 19:02:43 +0530

    docs(env): added example env and updated env schema

--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,8 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    SUPABASE_URL: z.string().url(),
+    SUPABASE_KEY: z.string(),
   },
 
   /**
@@ -25,6 +27,8 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
