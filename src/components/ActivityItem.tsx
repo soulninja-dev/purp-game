0b57182b4ff3d2875f7c577aa2b4b63510509a77
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-09-26 20:07:56 +0530

    feat(activity): added component for activity page items, common for both tabs

---
+++ src/components/ActivityItem.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import { relativeTimeFrom } from "~/utils/utils";
+
+interface Props {
+  avatars: `https://${string}`[];
+  by: string;
+  to: string;
+  action: "like" | "recast";
+  points: number;
+  time: number;
+}
+
+const ActivityItem = ({ by, to, action, points, avatars, time }: Props) => {
+  return (
+    <div className="grid grid-cols-4 gap-2 text-gray-300">
+      <div className="flex">
+        {avatars.map((avatar, ind) => (
+          <Image
+            src={avatar}
+            className={
+              "h-11 w-11 rounded-full " + (ind === 1 ? "-translate-x-4" : null)
+            }
+            key={ind}
+            width={44}
+            height={44}
+            alt="avatar"
+          />
+        ))}
+      </div>
+      <div className="col-span-3 flex flex-col gap-1">
+        <div className="">
+          <span className="font-semibold text-gray-100">{by}</span>{" "}
+          {action === "like" ? "liked " : "recasted "}
+          <span className="font-semibold text-gray-100">{to + "'s"}</span>{" "}
+          tweet.
+        </div>
+        <div className="flex items-center gap-2 text-sm">
+          <div>{relativeTimeFrom(time)}</div>
+          <div className="h-[3px] w-[3px] rounded-full bg-[#D9D9D9]"></div>
+          <div>
+            🟣{points} point{`${points > 1 ? "s" : ""}`}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ActivityItem;
