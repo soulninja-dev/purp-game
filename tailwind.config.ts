ea7fb0bd848e3a3aa1548eb52a8bedf632d562bb
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-09-26 08:33:59 +0530

    feat(colors): added colors according to styleguide to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ export default {
       fontFamily: {
         inter: ["Inter", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        gray: {
+          950: "#131315",
+          900: "#18181B",
+          600: "#5D5F61",
+          500: "#76787A",
+          400: "#909499",
+          300: "#AAAEB3",
+          100: "#E1E3E6",
+        },
+        farcaster: {
+          900: "#8465CB",
+        },
+        background: "#0A0A0A",
+      },
     },
   },
   plugins: [],
