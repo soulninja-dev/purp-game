316fa2516ed344b582616a402617a278cecba321
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-09-26 20:08:44 +0530

    refactor: added dummy data to activity page for dev

--- src/pages/activity.tsx
+++ src/pages/activity.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useState } from "react";
+import ActivityItem from "~/components/ActivityItem";
 import Navbar from "~/components/Navbar";
 
 const Activity = () => {
@@ -39,6 +40,96 @@ const Activity = () => {
               }`
             }
           ></div>
+        </div>
+        <div className="flex flex-col gap-4 py-2">
+          <ActivityItem
+            avatars={[
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226836121002004/image.png?ex=651433a7&is=6512e227&hm=08dc1ef7293cf8a1e41b6cc4c2e6f743a09c01942772a43648ac080b17abebfe&",
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226801849340015/image.png?ex=6514339f&is=6512e21f&hm=63a43f78dd48e3693fa6623cf523bdfb12b945f76a56594b9d5e08abebfecb23&",
+            ]}
+            by="You"
+            to="elonmusk"
+            action="like"
+            points={1}
+            time={(new Date() as unknown as number) - 3600 * 1000}
+          />
+          <ActivityItem
+            avatars={[
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226836121002004/image.png?ex=651433a7&is=6512e227&hm=08dc1ef7293cf8a1e41b6cc4c2e6f743a09c01942772a43648ac080b17abebfe&",
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226801849340015/image.png?ex=6514339f&is=6512e21f&hm=63a43f78dd48e3693fa6623cf523bdfb12b945f76a56594b9d5e08abebfecb23&",
+            ]}
+            by="You"
+            to="elonmusk"
+            action="like"
+            points={1}
+            time={(new Date() as unknown as number) - 3600 * 1000}
+          />
+          <ActivityItem
+            avatars={[
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226836121002004/image.png?ex=651433a7&is=6512e227&hm=08dc1ef7293cf8a1e41b6cc4c2e6f743a09c01942772a43648ac080b17abebfe&",
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226801849340015/image.png?ex=6514339f&is=6512e21f&hm=63a43f78dd48e3693fa6623cf523bdfb12b945f76a56594b9d5e08abebfecb23&",
+            ]}
+            by="corbin.eth"
+            to="elonmusk"
+            action="like"
+            points={1}
+            time={(new Date() as unknown as number) - 3600 * 1000}
+          />
+          <ActivityItem
+            avatars={[
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226836121002004/image.png?ex=651433a7&is=6512e227&hm=08dc1ef7293cf8a1e41b6cc4c2e6f743a09c01942772a43648ac080b17abebfe&",
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226801849340015/image.png?ex=6514339f&is=6512e21f&hm=63a43f78dd48e3693fa6623cf523bdfb12b945f76a56594b9d5e08abebfecb23&",
+            ]}
+            by="corbin.eth"
+            to="elonmusk"
+            action="like"
+            points={1}
+            time={(new Date() as unknown as number) - 3600 * 1000}
+          />
+          <ActivityItem
+            avatars={[
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226836121002004/image.png?ex=651433a7&is=6512e227&hm=08dc1ef7293cf8a1e41b6cc4c2e6f743a09c01942772a43648ac080b17abebfe&",
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226879397822494/image.png?ex=651433b1&is=6512e231&hm=e86ae311325e3be5e3b120835ed0280ce09f91beca54b8c3e8bd082dc5c45bb0&",
+            ]}
+            by="You"
+            to="ogechukwu.eth"
+            action="recast"
+            points={3}
+            time={(new Date() as unknown as number) - 3600 * 1000}
+          />
+          <ActivityItem
+            avatars={[
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226836121002004/image.png?ex=651433a7&is=6512e227&hm=08dc1ef7293cf8a1e41b6cc4c2e6f743a09c01942772a43648ac080b17abebfe&",
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226879397822494/image.png?ex=651433b1&is=6512e231&hm=e86ae311325e3be5e3b120835ed0280ce09f91beca54b8c3e8bd082dc5c45bb0&",
+            ]}
+            by="You"
+            to="elonmusk"
+            action="recast"
+            points={3}
+            time={(new Date() as unknown as number) - 3600 * 1000}
+          />
+          <ActivityItem
+            avatars={[
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226836121002004/image.png?ex=651433a7&is=6512e227&hm=08dc1ef7293cf8a1e41b6cc4c2e6f743a09c01942772a43648ac080b17abebfe&",
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226879397822494/image.png?ex=651433b1&is=6512e231&hm=e86ae311325e3be5e3b120835ed0280ce09f91beca54b8c3e8bd082dc5c45bb0&",
+            ]}
+            by="You"
+            to="elonmusk"
+            action="recast"
+            points={3}
+            time={(new Date() as unknown as number) - 3600 * 1000}
+          />
+          <ActivityItem
+            avatars={[
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226836121002004/image.png?ex=651433a7&is=6512e227&hm=08dc1ef7293cf8a1e41b6cc4c2e6f743a09c01942772a43648ac080b17abebfe&",
+              "https://cdn.discordapp.com/attachments/856193656569462824/1156226801849340015/image.png?ex=6514339f&is=6512e21f&hm=63a43f78dd48e3693fa6623cf523bdfb12b945f76a56594b9d5e08abebfecb23&",
+            ]}
+            by="You"
+            to="elonmusk"
+            action="like"
+            points={1}
+            time={(new Date() as unknown as number) - 3600 * 1000}
+          />
         </div>
       </main>
       <Navbar />
