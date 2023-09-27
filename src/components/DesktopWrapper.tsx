76b769631018cbe741e7e620985c0421948619e1
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-09-27 21:16:29 +0530

    feat(wrapper): added wrapper to make desktop ui look like mobile

---
+++ src/components/DesktopWrapper.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode | ReactNode[];
+}
+
+const DesktopWrapper = ({ children }: Props) => {
+  return (
+    <div className="flex w-full items-center justify-center bg-background">
+      <div className="w-full max-w-sm">{children}</div>
+    </div>
+  );
+};
+
+export default DesktopWrapper;
