6940f945034fcde03f1284cceb8a30b985f9d8e4
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-09-27 21:46:24 +0530

    style(types): added types for server

---
+++ src/utils/types.ts
@@ -0,0 +1,9 @@
+export type Action = {
+  senderAvatarUrl: `https://${string}`;
+  recipientAvatarUrl: `https://${string}`;
+  senderName: string;
+  recipientName: string;
+  actionDisplayWording: string;
+  pointAmount: number;
+  actionTimestamp: number;
+};
