0f1bb9c2427e2d7f479b190629518c78a6cdd0d6
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-09-27 22:19:38 +0530

    feat(loading): improved loading state

--- src/pages/activity.tsx
+++ src/pages/activity.tsx
@@ -10,10 +10,6 @@ const Activity = () => {
 
   const { data: actions } = api.actions.getAllActions.useQuery();
 
-  if (!actions) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <>
       <Head>
@@ -52,19 +48,43 @@ const Activity = () => {
               }
             ></div>
           </div>
-          <div className="flex flex-col gap-4 py-2">
-            {actions?.map((item: ActionProps, index: number) => (
-              <ActivityItem
-                key={index}
-                avatars={item.avatars}
-                by={item.by}
-                to={item.to}
-                action={item.action}
-                points={item.points}
-                time={item.time}
-              />
-            ))}
-          </div>
+          {!actions ? (
+            <div className="flex h-[calc(100vh-172.8px)] items-center justify-center">
+              <svg
+                className="-ml-1 mr-3 h-5 w-5 animate-spin"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            </div>
+          ) : (
+            <div className="flex flex-col gap-4 py-2">
+              {actions?.map((item: ActionProps, index: number) => (
+                <ActivityItem
+                  key={index}
+                  avatars={item.avatars}
+                  by={item.by}
+                  to={item.to}
+                  action={item.action}
+                  points={item.points}
+                  time={item.time}
+                />
+              ))}
+            </div>
+          )}
         </main>
         <Navbar />
       </DesktopWrapper>
