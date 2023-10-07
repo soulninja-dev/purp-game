b6819c2672b2a80fa09f6e7be2c6f65a7e59848a
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-10-07 15:16:15 +0530

    refactor: converted spinner to component

--- src/pages/activity.tsx
+++ src/pages/activity.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import ActivityItem, { type ActionProps } from "~/components/ActivityItem";
 import DesktopWrapper from "~/components/DesktopWrapper";
 import Navbar from "~/components/Navbar";
+import Spinner from "~/components/Spinner";
 import { api } from "~/utils/api";
 
 const Activity = () => {
@@ -20,56 +21,8 @@ const Activity = () => {
       <DesktopWrapper>
         <main className="flex min-h-screen flex-col gap-3 bg-background px-5 py-6 font-inter text-gray-300">
           <div className="text-xl font-medium">All Activity</div>
-          {/* <div className="relative flex items-center gap-8 border-b border-gray-900">
-            <button
-              onClick={() => setTab("activity")}
-              className={`${
-                tab === "activity" ? "text-farcaster-900" : null
-              } py-1`}
-            >
-              All Activity
-            </button>
-            <button
-              onClick={() => setTab("friends")}
-              className={`${
-                tab === "friends" ? "text-farcaster-900" : null
-              } py-1`}
-            >
-              Friends
-            </button>
-            <div
-              className={
-                "absolute bottom-0 h-0.5 bg-farcaster-900 transition-all duration-300 ease-in-out " +
-                `${
-                  tab === "friends"
-                    ? "w-14 translate-x-32"
-                    : "w-24 translate-x-0"
-                }`
-              }
-            ></div>
-          </div> */}
           {!actions ? (
-            <div className="flex h-[calc(100vh-172.8px)] items-center justify-center">
-              <svg
-                className="-ml-1 mr-3 h-5 w-5 animate-spin"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            </div>
+            <Spinner className="h-[calc(100vh-172.8px)]" />
           ) : (
             <div className="flex flex-col gap-4 py-2">
               {actions?.map((item: ActionProps, index: number) => (
