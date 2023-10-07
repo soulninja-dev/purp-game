9539935fe84d473c65bb17bd1774ba5f2350e249
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-10-07 15:45:42 +0530

    feat(p2r): added pull to refresh functionality to activity page

--- src/pages/activity.tsx
+++ src/pages/activity.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-// import { useState } from "react";
+import PullToRefresh from "react-simple-pull-to-refresh";
 import ActivityItem, { type ActionProps } from "~/components/ActivityItem";
 import DesktopWrapper from "~/components/DesktopWrapper";
 import Navbar from "~/components/Navbar";
@@ -7,9 +7,7 @@ import Spinner from "~/components/Spinner";
 import { api } from "~/utils/api";
 
 const Activity = () => {
-  // const [tab, setTab] = useState<"activity" | "friends">("activity");
-
-  const { data: actions } = api.actions.getAllActions.useQuery();
+  const { data: actions, refetch } = api.actions.getAllActions.useQuery();
 
   return (
     <>
@@ -18,29 +16,39 @@ const Activity = () => {
         <meta name="description" content="p2p creator rewards for farcaster" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <DesktopWrapper>
-        <main className="flex min-h-screen flex-col gap-3 bg-background px-5 py-6 font-inter text-gray-300">
-          <div className="text-xl font-medium">All Activity</div>
-          {!actions ? (
-            <Spinner className="h-[calc(100vh-172.8px)]" />
-          ) : (
-            <div className="flex flex-col gap-4 py-2">
-              {actions?.map((item: ActionProps, index: number) => (
-                <ActivityItem
-                  key={index}
-                  avatars={item.avatars}
-                  by={item.by}
-                  to={item.to}
-                  action={item.action}
-                  points={item.points}
-                  time={item.time}
-                />
-              ))}
-            </div>
-          )}
-        </main>
-        <Navbar />
-      </DesktopWrapper>
+      <PullToRefresh
+        onRefresh={async () => {
+          await refetch();
+          return;
+        }}
+        backgroundColor="#0A0A0A"
+        refreshingContent={<Spinner />}
+        pullingContent={<Spinner />}
+      >
+        <DesktopWrapper>
+          <main className="flex min-h-screen flex-col gap-3 bg-background px-5 py-6 font-inter text-gray-300">
+            <div className="text-xl font-medium">All Activity</div>
+            {!actions ? (
+              <Spinner className="h-[calc(100vh-172.8px)]" />
+            ) : (
+              <div className="flex flex-col gap-4 py-2">
+                {actions?.map((item: ActionProps, index: number) => (
+                  <ActivityItem
+                    key={index}
+                    avatars={item.avatars}
+                    by={item.by}
+                    to={item.to}
+                    action={item.action}
+                    points={item.points}
+                    time={item.time}
+                  />
+                ))}
+              </div>
+            )}
+          </main>
+          <Navbar />
+        </DesktopWrapper>
+      </PullToRefresh>
     </>
   );
 };
