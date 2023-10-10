925cc1ef5058df4390e816c50b882d529c127324
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-10-10 19:15:45 +0530

    fix: updated middleware to allow assets and profile page

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { NextResponse, type NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   if (
     !request.nextUrl.pathname.startsWith("/leaderboard") &&
+    !request.nextUrl.pathname.startsWith("/profile") &&
     !request.nextUrl.pathname.startsWith("/activity")
   )
     return NextResponse.redirect(new URL("/leaderboard", request.url));
@@ -19,7 +20,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|usdc.png).*)",
     "/",
   ],
 };
