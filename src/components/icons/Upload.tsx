011cf0609e390aaae7ac7a8ba9c17a199a8b7aef
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-10-10 19:14:10 +0530

    refactor(resources): added icons and assets

---
+++ src/components/icons/Upload.tsx
@@ -0,0 +1,28 @@
+const UploadIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M12 20.25L12 3.75"
+        stroke="#BBA9E2"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M5.25 10.5L12 3.75L18.75 10.5"
+        stroke="#BBA9E2"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default UploadIcon;
