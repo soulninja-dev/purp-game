616ba8c2dbcc02e32a3e89075c7e782917658ff6
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-10-11 20:02:19 +0530

    refactor: updated desktopwrapper for 404 page

--- src/components/DesktopWrapper.tsx
+++ src/components/DesktopWrapper.tsx
@@ -2,11 +2,16 @@ import type { ReactNode } from "react";
 
 interface Props {
   children?: ReactNode | ReactNode[];
+  noPaddingBottom?: boolean;
 }
 
-const DesktopWrapper = ({ children }: Props) => {
+const DesktopWrapper = ({ children, noPaddingBottom = false }: Props) => {
   return (
-    <div className="flex w-full items-center justify-center bg-background pb-10">
+    <div
+      className={`flex w-full items-center justify-center bg-background ${
+        !noPaddingBottom ? "pb-10" : ""
+      }`}
+    >
       <div className="w-full max-w-sm">{children}</div>
     </div>
   );
