3a34148f642baf968f150a7ffd5456335a0ef4f9
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-10-17 21:02:36 +0530

    fix: update types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,6 @@
 export type Action = {
-  senderAvatarUrl: `https://${string}`;
-  recipientAvatarUrl: `https://${string}`;
+  senderAvatarUrl: Url;
+  recipientAvatarUrl: Url;
   senderName: string;
   recipientName: string;
   actionDisplayWording: string;
@@ -8,12 +8,25 @@ export type Action = {
   actionTimestamp: number;
 };
 
-export type LBUser = {
+export type BaseLBUser = {
   username: string;
-  userfid: number;
   userdisplayname: string;
-  useravatarurl: `https://${string}`;
+  useravatarurl: Url;
+};
+
+export type LBUser = BaseLBUser & {
+  userfid: number;
   points: number;
   usdcamount: number;
-  userUrl: `https://${string}`;
+  userUrl: Url;
+};
+
+type Url = `${"https" | "http"}://${string}`;
+
+export type Post = {
+  content: string;
+  attachments?: Url[];
+  likes: number;
+  recasts: number;
+  comments: number;
 };
