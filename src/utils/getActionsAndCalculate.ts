c1c918d34fa081dfaec90af81bbbec892dc22fbd
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-10-18 15:02:51 +0530

    fix(backend): fixed the entire file, types, code, syntax, weird coed quality, and removed lodash

--- src/utils/getActionsAndCalculate.ts
+++ src/utils/getActionsAndCalculate.ts
@@ -1,18 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
-// TODO: @soulninja-dev - remove lodash
-// TODO: @pybash1       - fix types
-
-import _ from "lodash";
 import { createClient } from "@supabase/supabase-js";
 import { fnames } from "./fnames";
 const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
 import { env } from "~/env.mjs";
-import type { LBUser } from "./types";
+import type {
+  ActionsData,
+  LBUser,
+  NeynarResponse,
+  PaymagicResponse,
+} from "./types";
 
 export async function getActionsAndCalculate() {
   const date = new Date().toISOString().slice(0, 10);
@@ -23,36 +18,42 @@ export async function getActionsAndCalculate() {
     try {
       const neynarResponse = await getRecentReactionsfromFC(fname, date);
 
-      const actionsData: any[] = _.get(
-        neynarResponse,
-        "query_result.data.rows",
-        [],
-      );
+      const actionsData = neynarResponse.query_result.data.rows || [];
 
-      const dailyPurpTotal: number = _.sumBy(actionsData, (o) =>
-        Number(o.pointAmount),
-      );
+      let dailyPurpTotal = 0;
+
+      actionsData.forEach((action) => {
+        dailyPurpTotal += Number(action.pointAmount);
+      });
 
       console.log(`Fname: ${fname}`);
       console.log(`Action count: ${actionsData.length}`);
       console.log(`🟣 Points sent: ${dailyPurpTotal}`);
       console.log(`---`);
-      if (!_.isEmpty(actionsData)) {
-        for (const action of actionsData) {
-          action.day = new Date(action.actionTimestamp)
+      actionsData.forEach((action) => {
+        const handle = async () => {
+          const action_: NeynarResponse["query_result"]["data"]["rows"][0] & {
+            day?: string;
+            recipientAddress?: string;
+            usdcAmt?: number;
+          } = { ...action };
+
+          action_.day = new Date(action.actionTimestamp)
             .toISOString()
             .split("T")[0];
-          action.recipientAddress = _.get(
-            await getUserAddress(action.recipientFid),
-            "users.0.accountAddress",
-            "0x",
-          );
-          action.usdcAmt =
+
+          action_.recipientAddress =
+            (await getUserAddress(action.recipientFid)).users[0]
+              ?.accountAddress ?? "0x";
+
+          action_.usdcAmt =
             (dailyUsdcAmt * Number(action.pointAmount)) / dailyPurpTotal;
 
-          await upsertAction(action);
-        }
-      }
+          await upsertAction(action_);
+        };
+
+        void handle();
+      });
     } catch (error) {
       console.error(`Error processing fname: ${fname}`);
       console.error(error);
@@ -60,15 +61,14 @@ export async function getActionsAndCalculate() {
   }
 }
 
-async function getRecentReactionsfromFC(
-  fname: string,
-  day: string,
-): Promise<any> {
+async function getRecentReactionsfromFC(fname: string, day: string) {
   const endpoint =
     "https://data.hubs.neynar.com/api/queries/16/results?api_key=pxUz3WR2RjR4miAuzBoHDuRRmyJjQqsU5LKiITFQ";
+
   const headers: Record<string, string> = {
     "Content-Type": "application/json",
   };
+
   const parameters = {
     parameters: {
       fname: fname,
@@ -87,16 +87,14 @@ async function getRecentReactionsfromFC(
       throw new Error(`FC API returned an error: ${response.statusText}`);
     }
 
-    return await response.json();
+    return (await response.json()) as NeynarResponse;
   } catch (error) {
     console.error("Error fetching recent reactions from FC:", error);
     throw error;
   }
 }
 
-export async function getUserAddress(
-  fid: string,
-): Promise<{ users: { accountAddress: string }[] }> {
+export async function getUserAddress(fid: number) {
   const endpoint = `https://paymagicapi.com/v1/resolver`;
 
   const headers = {
@@ -120,7 +118,7 @@ export async function getUserAddress(
       );
     }
 
-    return await response.json();
+    return (await response.json()) as PaymagicResponse;
   } catch (error) {
     console.error("Paymagic Resolver: Error calling resolver", error);
     throw error;
@@ -151,18 +149,20 @@ const calculatePoints = async (fname: string, type: string) => {
   return { points, error: null };
 };
 
-export async function getUserData(fid: string): Promise<{
-  name: string;
-  avatarUrl: string;
-  pointsSent: number;
-  pointsEarned: number;
-}> {
+export async function getUserData(fid: string) {
   try {
-    const { data, error } = await supabase
+    const {
+      data,
+      error,
+    }: {
+      data: { senderDisplayName: string; senderAvatarUrl: string }[] | null;
+      error: unknown;
+    } = await supabase
       .from("actions")
       .select("senderDisplayName, senderAvatarUrl")
       .eq("senderName", fid)
       .limit(1);
+
     if (error) throw error;
 
     const { points: pointsSent, error: patronError } = await calculatePoints(
@@ -175,11 +175,13 @@ export async function getUserData(fid: string): Promise<{
       fid,
       "recipient",
     );
+
     if (patronError) throw patronError;
+    if (earnerError) throw earnerError;
 
     const res = {
-      name: data[0]?.senderDisplayName,
-      avatarUrl: data[0]?.senderAvatarUrl,
+      name: data![0]?.senderDisplayName,
+      avatarUrl: data![0]?.senderAvatarUrl,
       pointsSent,
       pointsEarned,
     };
@@ -191,14 +193,14 @@ export async function getUserData(fid: string): Promise<{
   }
 }
 
-async function upsertAction(actionData: any): Promise<any> {
+async function upsertAction(actionData: ActionsData) {
   try {
     const SUPABASE_URL = "https://ixyadahmfkomzjqwzwax.supabase.co";
     const SUPABASE_KEY =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4eWFkYWhtZmtvbXpqcXd6d2F4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NjQ0OTU2MiwiZXhwIjoyMDEyMDI1NTYyfQ.3Q4kwoXafKICeBLMT5ep-fOvgYJfogPcnD1bGs0itMA";
     const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-    _.unset(actionData, "usdcAmt");
+    delete actionData.usdcAmt;
 
     const { data, error } = await supabase.from("actions").upsert([actionData]);
 
