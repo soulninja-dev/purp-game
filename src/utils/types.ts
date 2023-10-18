9452201c85e83299b0f22e15674c38c1843b77d9
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-10-18 14:59:26 +0530

    fix(types): added types for backend files

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -22,6 +22,9 @@ export type LBUser = BaseLBUser & {
 };
 
 type Url = `${"https" | "http"}://${string}`;
+type DateTimeZ =
+  `${number}-${number}-${number}T${number}:${number}:${number}.${number}Z`;
+type DateTime = `${number}-${number}-${number}T${number}:${number}:${number}`;
 
 export type Post = {
   content: string;
@@ -29,4 +32,53 @@ export type Post = {
   likes: number;
   recasts: number;
   comments: number;
+};
+
+export type NeynarResponse = {
+  query_result: {
+    retrieved_at: DateTimeZ;
+    query_has: string;
+    query: string;
+    runtime: number;
+    data: {
+      rows: {
+        contentUrl: Url;
+        senderFid: number;
+        recipientDisplayName: string;
+        senderAvatarUrl: Url;
+        senderDisplayName: string;
+        actionDisplayWording: string;
+        recipientFid: number;
+        actionType: string;
+        actionId: string;
+        pointAmount: string;
+        recipientName: string;
+        actionTimestamp: DateTime;
+        recipientAvatarUrl: Url;
+        senderName: string;
+        reactionHash: string;
+      }[];
+      columns: {
+        friendly_name: string;
+        type: string | null;
+        name: string;
+      }[];
+    };
+    id: number;
+    data_source_id: number;
+  };
+};
+
+export type ActionsData = NeynarResponse["query_result"]["data"]["rows"][0] & {
+  day?: string;
+  recipientAddress?: string;
+  usdcAmt?: number;
+};
+
+export type PaymagicResponse = {
+  updatedAt: DateTimeZ;
+  users: {
+    userId: string;
+    accountAddress: string;
+  }[];
 };
