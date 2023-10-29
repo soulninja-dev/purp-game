1797a7c0de5b161d9be05160189505003da49842
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-10-29 16:20:06 +0530

    style: updated imports

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getActionsAndCalculate } from "~/utils/getActionsAndCalculate";
+import { getActionsAndCalculate } from "~/utils/farcaster";
 
 export const config = {
   runtime: "edge",
